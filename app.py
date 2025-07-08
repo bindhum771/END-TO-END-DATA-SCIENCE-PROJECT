a815eb3f4a8c213339c592df3f650ee78d24a60c
bindhum771 <123546986+bindhum771@users.noreply.github.com>
2025-07-08 14:49:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template, request
-import joblib
+from flask import Flask, request, render_template
+import pickle
 import numpy as np
-from datasets import load_dataset
-
-ds = load_dataset("scikit-learn/iris")
 
 app = Flask(__name__)
-model = joblib.load('model/iris_model.pkl')
+model = pickle.load(open('C:\\Users\\HP\Downloads\\customer_churn_project\\customer_churn_project\\churn_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    prediction = model.predict([np.array(features)])
+    churn_label = 'Yes' if prediction[0] == 1 else 'No'
+    return render_template('index.html', prediction_text=f"Churn Prediction: {churn_label}")
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    prediction = None
-    if request.method == 'POST':
-        features = [float(request.form[f]) for f in ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-        prediction = model.predict([features])[0]
-    return render_template('index.html', prediction=prediction)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
